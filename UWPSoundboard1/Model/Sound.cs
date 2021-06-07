9d56fee02737dd28e842a896ce67efd14435399e
SanthosiKondupalli <santhosi.kondupalli@gmail.com>
2021-06-06 21:59:58 -0700

    Initial commit

---
+++ UWPSoundboard1/Model/Sound.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundboard1.Model
+{
+    public enum SoundCategory
+    {
+        Animals,
+        Cartoons,
+        Taunts,
+        Warnings
+    }
+    public class Sound
+    {
+        public String Name{get; set;}
+        public SoundCategory CategoryName { get; set;}
+        public String ImageLink { get; set; }
+        public String SoundLink { get; set; }
+    }
+
+}
